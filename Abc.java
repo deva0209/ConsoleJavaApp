8cfeabaaddacad4bce75f27f03875e1415e24812
srimadhuri-k <madhurireddy.yelluri@gmail.com>
2022-12-18 20:53:17 +0530

    second line added

--- Abc.java
+++ Abc.java
@@ -1,5 +1,6 @@
 class Abc {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java!"); 
+        System.out.println("Welcome to Java!");
+        system.out.println("Hello all"); 
     }
 }
