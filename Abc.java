bab736e57df90bfea59f8cab20cfb04c0b8ce68b
deva0209 <sgnandev@gmail.com>
2022-12-23 22:26:43 +0530

    3rd commit

--- Abc.java
+++ Abc.java
@@ -1,6 +1,6 @@
 class Abc {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
-        system.out.println("Hello all"); 
+        system.out.println("second line"); 
     }
 }
